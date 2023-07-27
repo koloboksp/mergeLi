de711b068a7dcab6bef67f628a45fa35c5064589
michael <m.drogman@gmail.com>
2023-07-27 11:40:55 +0300

    add NoPath animation

--- Assets/Art/Animations/BallAnimator.cs
+++ Assets/Art/Animations/BallAnimator.cs
@@ -1,21 +1,23 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using Core;
 
 public class BallAnimator : MonoBehaviour
 {
-    private const float FADE = .2f;
-    
-    [SerializeField] private Core.DefaultBallSkin ballSkin;
+    private const float CROSS_FADE = .1f;
+
+    private Ball ball;
+    [SerializeField] private DefaultBallSkin ballSkin;
     [SerializeField] private Animation anim;
 
     [SerializeField] private AnimationClip idle;
     [SerializeField] private AnimationClip select;
     [SerializeField] private AnimationClip move;
-
+    [SerializeField] private AnimationClip noPath;
     [SerializeField] private AnimationClip[] idles;
 
-    private Dictionary<Core.DefaultBallSkin.BallState, AnimationClip> clips;
+    private Dictionary<DefaultBallSkin.BallState, AnimationClip> clips;
 
     private void Awake()
     {
@@ -24,27 +26,40 @@ public class BallAnimator : MonoBehaviour
 
         ballSkin.ChangeStateEvent += SetAnimationState;
 
-        clips = new Dictionary<Core.DefaultBallSkin.BallState, AnimationClip>
+        ball = GetComponentInParent<Ball>();
+        if (ball != null)
+            ball.OnPathNotFound += Ball_OnPathNotFound;
+
+        clips = new Dictionary<DefaultBallSkin.BallState, AnimationClip>
         {
-            { Core.DefaultBallSkin.BallState.Idle, idle },
-            { Core.DefaultBallSkin.BallState.Select, select },
-            { Core.DefaultBallSkin.BallState.Move, move },
-            { Core.DefaultBallSkin.BallState.PathNotFound, move }
+            { DefaultBallSkin.BallState.Idle, idle },
+            { DefaultBallSkin.BallState.Select, select },
+            { DefaultBallSkin.BallState.Move, move },
+            { DefaultBallSkin.BallState.PathNotFound, move }
         };
 
         anim[idle.name].normalizedTime = Random.Range(0, 1f);
+    }
+
+    private void Ball_OnPathNotFound()
+    {
+        anim.CrossFade(noPath.name, CROSS_FADE);
+        anim.CrossFadeQueued(select.name, CROSS_FADE);
     }
 
     private void OnDestroy()
     {
         if (ballSkin != null)
             ballSkin.ChangeStateEvent -= SetAnimationState;
+
+        if (ball != null)
+            ball.OnPathNotFound -= Ball_OnPathNotFound;
     }
 
-    private void SetAnimationState(Core.DefaultBallSkin.BallState ballState)
+    private void SetAnimationState(DefaultBallSkin.BallState ballState)
     {
         string clipName = clips[ballState] != null ? clips[ballState].name : idle.name;
 
-        anim.CrossFade(clipName, FADE);
+        anim.CrossFade(clipName, CROSS_FADE);
     }
 }
