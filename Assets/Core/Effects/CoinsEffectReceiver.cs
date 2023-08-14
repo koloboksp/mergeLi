d516ae0abab9d0123c10274d2a0cfdb03f414adc
koloboksp <m.koloboksp@gmail.com>
2023-08-14 20:02:38 +0300

    coins collect effect added

---
+++ Assets/Core/Effects/CoinsEffectReceiver.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Core.Effects
+{
+    public class CoinsEffectReceiver : MonoBehaviour
+    {
+    
+    }
+}
--- Assets/Core/Effects/CollapsePointsEffect.cs
+++ Assets/Core/Effects/CollapsePointsEffect.cs
@@ -1,18 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class CollapsePointsEffect : MonoBehaviour
+namespace Core.Effects
 {
-    [SerializeField] private Animation _animation;
-    [SerializeField] private AnimationClip _clip;
-    [SerializeField] private Text _points;
-
-    public float Duration => _clip.length;
-    
-    public void Run(int points)
+    public class CollapsePointsEffect : MonoBehaviour
     {
-        _points.text = points.ToString();
+        [SerializeField] private Text _points;
+        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private GameObject _coinPrefab;
+        [SerializeField] private AnimationCurve _movingSpeed;
+
+        private List<(GameObject item, Vector3 startPosition, Vector3 endPosition, float delay)> _coins = new ();
+    
+        public float Duration => _duration;
+    
+        public void Run(int points)
+        {
+            _points.text = points.ToString();
+
+            var coinsEffectReceiver = GameObject.FindObjectOfType<CoinsEffectReceiver>();
+
+            var coin = GameObject.Instantiate(_coinPrefab, transform);
+            _coins.Add((coin, coin.transform.position, coinsEffectReceiver.transform.position, 0.0f));
+
+            StartCoroutine(StartFx(_duration));
+        }
+
+        IEnumerator StartFx(float time)
+        {
+            var timer = 0.0f;
         
-        Destroy(this.gameObject, Duration);
+            while (timer < time)
+            {
+                foreach (var coin in _coins)
+                {
+                    var pathParam = timer / time;
+                    coin.item.transform.position = Vector3.Lerp(coin.startPosition, coin.endPosition, _movingSpeed.Evaluate(pathParam));
+                }
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            foreach (var coin in _coins)
+            {
+                Destroy(coin.item);
+            }
+            _coins.Clear();
+        
+            Destroy(gameObject);
+        }
     }
 }
