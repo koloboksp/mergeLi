9e8cf521db828d2c7f251999c76fbcbcbc32a6c1
koloboksp <m.koloboksp@gmail.com>
2023-11-06 23:26:08 +0300

    add locale

---
+++ Assets/Scripts/Common/Core/Base/math/Random.cs
@@ -0,0 +1,16 @@
+﻿namespace Atom
+{
+    public class Random
+    {
+        private static int mId = 1;
+
+        private readonly System.Random mRand = new(++mId);
+        public int Next() { return mRand.Next(); }
+
+        private static readonly System.Random mRandom = new(0);
+        public static int GetRandom()
+        {
+            return mRandom.Next();
+        }
+    }
+}
