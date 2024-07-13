5bc15bfd9cbbc9a9fc04a3c4717d9f4d0e2c0a24
koloboksp <m.koloboksp@gmail.com>
2024-07-14 00:14:47 +0300

    fix points out of screen error

--- Assets/Scripts/Steps/CustomOperations/UIFxLayer.cs
+++ Assets/Scripts/Steps/CustomOperations/UIFxLayer.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace Core.Steps.CustomOperations
 {
     public class UIFxLayer : MonoBehaviour
     {
-        
+        [SerializeField] private RectTransform _root;
+
+        public RectTransform Root => _root;
     }
 }
---
+++ Assets/Scripts/Steps/CustomOperations/UIStayInRect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Steps.CustomOperations
+{
+    public class UIStayInRect : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _targetRoot;
+
+        private static readonly List<UIFxLayer> NoAllocFoundLayers = new();
+        private static readonly Vector3[] NoAllocLayerCorners = new Vector3[4];
+        private static readonly Vector3[] NoAllocTargetCorners = new Vector3[4];
+        
+        private void Update()
+        {
+            GetComponentsInParent(false, NoAllocFoundLayers);
+            if (NoAllocFoundLayers.Count == 0)
+                return;
+            
+            NoAllocFoundLayers[0].Root.GetWorldCorners(NoAllocLayerCorners);
+            _targetRoot.parent.InverseTransformVectors(NoAllocLayerCorners);
+            _targetRoot.GetWorldCorners(NoAllocTargetCorners);
+            _targetRoot.parent.InverseTransformVectors(NoAllocTargetCorners);
+
+            var targetLeftBottomCorner = NoAllocTargetCorners[0];
+            var targetRightTopCorner = NoAllocTargetCorners[2];
+            var layerLeftBottomCorner = NoAllocLayerCorners[0];
+            var layerRightTopCorner = NoAllocLayerCorners[2];
+
+            if (targetLeftBottomCorner.x < layerLeftBottomCorner.x)
+            {
+                var localPosition = _targetRoot.localPosition;
+                localPosition.x += layerLeftBottomCorner.x - targetLeftBottomCorner.x;
+                _targetRoot.localPosition = localPosition;
+            }
+            if (targetLeftBottomCorner.y < layerLeftBottomCorner.y)
+            {
+                var localPosition = _targetRoot.localPosition;
+                localPosition.y += layerLeftBottomCorner.y - targetLeftBottomCorner.y;
+                _targetRoot.localPosition = localPosition;
+            }
+            if (targetRightTopCorner.x > layerRightTopCorner.x)
+            {
+                var localPosition = _targetRoot.localPosition;
+                localPosition.x -= (targetRightTopCorner.x - layerRightTopCorner.x);
+                _targetRoot.localPosition = localPosition;
+            }
+            if (targetRightTopCorner.y > layerRightTopCorner.y)
+            {
+                var localPosition = _targetRoot.localPosition;
+                localPosition.y -= (targetRightTopCorner.y - layerRightTopCorner.y);
+                _targetRoot.localPosition = localPosition;
+            }
+        }
+    }
+}
