8b1575fb094fa11958f4fa2888395afeabd65b9b
koloboksp <m.koloboksp@gmail.com>
2024-07-13 20:42:49 +0300

    set default active hat

---
+++ Assets/Scripts/Tutorials/Steps/SetActiveHatsTutorialStep.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Core.Tutorials
+{
+    public class SetActiveHatsTutorialStep : TutorialStep
+    {
+        [SerializeField] public string[] _activeHats;
+        [SerializeField] public Scene _scene;
+
+        protected override async Task<bool> InnerExecuteAsync(CancellationToken cancellationToken)
+        {
+            _scene.SetUserActiveHatsFilter(_activeHats);
+            
+            return true;
+        }
+    }
+}
