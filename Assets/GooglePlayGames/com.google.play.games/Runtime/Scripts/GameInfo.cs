1b28d9f97623824a75883a3b033c0b583b586a77
koloboksp <m.koloboksp@gmail.com>
2024-08-24 23:24:36 +0300

    add web oauth

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "533495091222"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "533495091222-efuti09iconnqsohg87r1e5cbdpje71i.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
